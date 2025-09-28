346e6f6364d3c96fecb6070124f64f677ebdd7ea
Aaryan <aaryany2505@gmail.com>
2025-09-28 20:02:25 +0530

    Connecting with nodejs

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const client = require("./client")
+const app = express()
+const axios = require("axios").default
+app.get("/",async(req,res)=>{
+    const cachedValue = await client.get('todos')
+    if(cachedValue) return res.json(JSON.parse(cachedValue))
+    
+const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos')
+await client.set("todos",JSON.stringify(data))
+await client.expire("todos",30)
+return res.json(data)
+})
+app.listen(9000)
